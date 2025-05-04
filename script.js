d04580e68adaf12763bed77a9e5bc47ff0fc3591
3roaa <roaaqarnia@gmail.com>
2025-05-04 18:36:53 +0300

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function switchLang(lang) {
-  const elements = document.querySelectorAll('[data-en]');
-  elements.forEach(el => {
+  document.querySelectorAll('[data-en]').forEach(el => {
     el.textContent = el.getAttribute(`data-${lang}`);
   });
-
-  // تغيير الاتجاه إذا كانت اللغة عربية
-  document.documentElement.dir = (lang === 'ar') ? 'rtl' : 'ltr';
-  document.body.style.textAlign = (lang === 'ar') ? 'right' : 'left';
+  document.body.dir = lang === 'ar' ? 'rtl' : 'ltr';
+  document.documentElement.lang = lang;
 }
-
