862f07fc15eb4ebc91088787714dacc341b67d3e
3roaa <roaaqarnia@gmail.com>
2025-05-04 19:46:42 +0300

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-// ملف script.js
-
-// دالة لتبديل اللغة
-function switchLang(language) {
-  // تغيير اللغة في الـ body
-  document.body.setAttribute('lang', language);
-
-  // تغيير النصوص في العناصر التي تحتوي على data-en و data-ar
-  const elements = document.querySelectorAll('.translatable');
-
-  elements.forEach(function(element) {
-    const enText = element.getAttribute('data-en');
-    const arText = element.getAttribute('data-ar');
-
-    if (language === 'ar') {
-      element.innerHTML = arText;
-    } else {
-      element.innerHTML = enText;
+// Function to switch between languages
+function switchLang(lang) {
+  const elements = document.querySelectorAll('[data-en], [data-ar]');
+  elements.forEach(element => {
+    const textEn = element.getAttribute('data-en');
+    const textAr = element.getAttribute('data-ar');
+    if (lang === 'en') {
+      element.textContent = textEn;
+    } else if (lang === 'ar') {
+      element.textContent = textAr;
     }
   });
 }
+
